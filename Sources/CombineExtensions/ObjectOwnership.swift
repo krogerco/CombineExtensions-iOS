2ec8ddb06ec36f10683959ff566b8576919b6fce
Isaac Ruiz <isaac.ruiz@kroger.com>
2023-03-03 15:20:35 -0800

    fixed docs

--- Sources/CombineExtensions/ObjectOwnership.swift
+++ Sources/CombineExtensions/ObjectOwnership.swift
@@ -1,6 +1,11 @@
 import Foundation
 
+// Retain strategy for objects in subscription operators
 public enum ObjectOwnership {
+
+    /// Strongly retains the object in the subscription
     case strong
+
+    /// Weakly retains the object in the subscription.
     case weak
 }
